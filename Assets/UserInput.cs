6b73609e7a769a529ce01b177ad1f3cbab48f003
TalmonG <118438762+TalmonG@users.noreply.github.com>
2023-05-18 18:18:44 +0100

    four fifty five

--- Assets/UserInput.cs
+++ Assets/UserInput.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UserInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static UserInput instance;
+
+    public Vector2 MoveInput { get; private set; }
+
+    public bool MenuOpenCloseImput { get; private set; }
+
+
+    private PlayerInput _playerInput;
+
+    private InputAction _moveAction;
+    private InputAction _menuOpenCloseAction;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        _playerInput = GetComponent<PlayerInput>();
+
+        SetupInputActions();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateInputs();
+    }
+
+    private void SetupInputActions()
+    {
+        _moveAction = _playerInput.actions["Move"];
+        _menuOpenCloseAction = _playerInput.actions["MenuOpenClose"];
+    }
+
+    private void UpdateInputs()
+    {
+        MoveInput = _moveAction.ReadValue<Vector2>();
+        MenuOpenCloseImput = _menuOpenCloseAction.WasPressedThisFrame();
     }
 }
